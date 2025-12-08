7e00fe709e0c887f5dabdec44f7ef9b897475ffc
akashgrao <raoakash1997@outlook.com>
2025-12-08 19:30:46 +0000

    Deploying to gh-pages from @ nirupamaps/nirupamaps.github.io@91e931833095cb22a708e0715d03caf3d882589e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "Work In Progress",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -43,13 +43,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/books/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "books-the-godfather",
           title: 'The Godfather',
