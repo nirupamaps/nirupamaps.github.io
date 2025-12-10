5feba626b72aabbc8c5093623037cb9b166f8844
nirupamaps <162002581+nirupamaps@users.noreply.github.com>
2025-12-10 19:53:01 +0000

    Deploying to gh-pages from @ nirupamaps/nirupamaps.github.io@ee14d43eabe7a5140774c3fb781b5dd8bdf09d84 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-calendar",
+          title: "calendar",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/calendar/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
